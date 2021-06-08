80521de6b9fa80442dcca6e6af584abdcdbcabba
vCamilx <72714741+vCamilx@users.noreply.github.com>
2021-06-08 16:02:02 -0500

    🪁 The game has been almost completely modified

--- src/com/github/vcamilx/javascript/first/project/SimpleGame.js
+++ src/com/github/vcamilx/javascript/first/project/SimpleGame.js
@@ -1,58 +1,54 @@
 const options = [
-    "rock",
-    "paper",
-    "scissors"
+    rock = { type: "rock", win: "scissors", lost: "paper" },
+    paper = { type: "paper", win: "rock", lost: "scissors" },
+    scissors = { type: "scissors", win: "paper", lost: "scissors" }
 ];
 
-let botRespone = options[Math.round(Math.random() * options.length)];
+let cpu = { name: "CPU", option: options[Math.round(Math.random() * options.length)] };
 
-let userOption = prompt(
-    "What do you want to choose? Rock | Paper | Scissors",
-    "Your choice."
-).toLowerCase();
+let user = function createUser() {
+    const name = prompt(
+        "What do you want your name to be in the game?",
+        "Write your name here!"
+    );
 
-switch (userOption) {
-    case "rock":
-        switch (botRespone) {
-            case "paper":
-                console.log(`You lost, your opponent drew ${botRespone}`);
-                break;
-            case "scissors":
-                console.log(`You win, your opponent drew ${botRespone}`);
-                break;
-            case "rock":
-                console.log(`Tie!, your opponent drew ${botRespone}`);
-                break;
-        }
-        break;
-    case "paper":
-        switch (botRespone) {
-            case "paper":
-                console.log(`Tie!, your opponent drew ${botRespone}`);
-                break;
-            case "scissors":
-                console.log(`You lost, your opponent drew ${botRespone}`);
-                break;
-            case "rock":
-                console.log(`You win, your opponent drew ${botRespone}`);
-                break;
-        }
-        break;
+    const result = prompt(
+        "What do you want to choose? Rock | Paper | Scissors",
+        "Write your choice here!"
+    ).toLowerCase();
 
-    case "scissors":
-        switch (botRespone) {
-            case "paper":
-                console.log(`You win, your opponent drew ${botRespone}`);
-                break;
-            case "scissors":
-                console.log(`Tie!, your opponent drew ${botRespone}`)
-                break;
-            case "rock":
-                console.log(`You lost, your opponent drew ${botRespone}`);
-                break;
-        }
-        break;
-    default:
-        console.error("You cannt do that, select an valid option");
-        break;
+    switch (result) {
+        case "rock":
+            return { name: name, option: options[0] };
+        case "paper":
+            return { name: name, option: options[1] };
+        case "scissors":
+            return { name: name, option: options[2] };
+        default:
+            return alert(`${result} isn't an valid option!`);;
+    };
 }
+
+const player = user();
+
+const messages = {
+    win: `${player.name} win, ${cpu.name} drew ${cpu.option.type}`,
+    lost: `${player.name} lost, ${cpu.name} drew ${cpu.option.type}`,
+    tie: `Tie!, ${cpu.name} drew ${cpu.option.type}`
+};
+
+function startGame() {
+    if (!(player.option.type === cpu.option.type)) {
+        if (cpu.option.type === player.option.lost) {
+            alert(messages.lost);
+            return;
+        } else {
+            alert(messages.win);
+            return;
+        }
+    };
+
+    return alert(messages.tie);
+};
+
+startGame();
